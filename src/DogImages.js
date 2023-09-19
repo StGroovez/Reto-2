87376c6a8a31c98f7383a00623839ecb76c3e5a3
StGroovez <abdo180294@gmail.com>
2023-09-19 15:54:58 -0600

    boris & friends

--- src/DogImages.js
+++ src/DogImages.js
@@ -1,28 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './DogImages.css'
+import chems from './pngwing.com.png';
 
 function DogImages() {
   const [dogImage, setDogImage] = useState('');
   const [favoriteImage, setFavoriteImage] = useState(null);
   const [selectedBreed, setSelectedBreed] = useState('');
   const [breedsList, setBreedsList] = useState([]);
+  const [isChemsVisible, setIsChemsVisible] = useState(true);
+
 
   useEffect(() => {
-    // Obtener lista de razas al cargar la aplicación
-    axios.get('https://dog.ceo/api/breeds/list/all')
-      .then(response => {
-        const breeds = Object.keys(response.data.message);
-        setBreedsList(breeds);
+      axios.get('https://dog.ceo/api/breeds/list/all')
+        .then(response => {
+          const breeds = Object.keys(response.data.message);
+          setBreedsList(breeds);
       })
-      .catch(error => {
-        console.error('Error fetching breeds:', error);
+        .catch(error => {
+          console.error('Error fetching breeds:', error);
       });
-  }, []);
+      setIsChemsVisible(true);
+
+  }, []); // Dependencia vacía para ejecutar una vez al inicio
 
   const fetchRandomImage = () => {
-    const apiUrl = selectedBreed
-      ? `https://dog.ceo/api/breed/${selectedBreed}/images/random`
-      : 'https://dog.ceo/api/breeds/image/random';
+    setIsChemsVisible(false);
+    const apiUrl = 
+      selectedBreed ? `https://dog.ceo/api/breed/${selectedBreed}/images/random`: 'https://dog.ceo/api/breeds/image/random';
 
     axios.get(apiUrl)
       .then(response => {
@@ -38,30 +43,38 @@ function DogImages() {
   };
 
   return (
-    <div>
-      <h1>Imágenes Aleatorias de Perros</h1>
-      <div>
-        <button onClick={fetchRandomImage}>Obtener imagen aleatoria</button>
-        <select
-          onChange={e => setSelectedBreed(e.target.value)}
-          value={selectedBreed}
+    <div className='dog-images'>
+      
+      <div className='header'>Boris & Friends</div>
+      
+      {isChemsVisible && <img className='chems-img' src={chems} alt="Chems" />}
+      
+
+        <div>
+          {dogImage && (
+            <div className='dog-img'>
+              <img className='real-img' src={dogImage} alt="Perro aleatorio" width="300" height="300" />
+              <button className='favorite' onClick={markAsFavorite}>Fav</button>
+            </div>      
+          )}          
+        </div>
+
+      <select 
+        className='breed-selector'
+        onChange={e => setSelectedBreed(e.target.value)}
+        value={selectedBreed}
         >
-          <option value="">Raza Aleatoria</option>
-          {breedsList.map(breed => (
-            <option key={breed} value={breed}>{breed}</option>
-          ))}
-        </select>
-        <button onClick={fetchRandomImage}>Obtener imagen por raza</button>
-      </div>
-      <div>
-        {dogImage && (
-          <div>
-            <img src={dogImage} alt="Perro aleatorio" width="300" height="300" />
-            <p>Enlace para copiar: {dogImage}</p>
-            <button onClick={markAsFavorite}>Marcar como favorita</button>
-          </div>
-        )}
-      </div>
+        <option value="">Raza Aleatoria</option>
+        {breedsList.map(breed => (
+          <option key={breed} value={breed}>{breed}</option>
+        ))}
+      </select>
+
+      <div className='buttons-container'>
+          <button  className='button-random' onClick={fetchRandomImage}>Obtener Perrito</button>
+        </div>
+      
+
       <div>
         {favoriteImage && (
           <div>
@@ -70,6 +83,7 @@ function DogImages() {
           </div>
         )}
       </div>
+      
     </div>
   );
 }
